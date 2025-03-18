c0e7505fb136f9f045d339400c54563dd4385816
BEA40 <hazelangulog04@gmail.com>
2025-03-18 00:00:43 -0600

    Crea archivo

---
+++ README.md
@@ -0,0 +1,5 @@
+JUEGO DE AMIGO SECRETO
+
+Este juego permite agregar nombres de personas para que luego se haga un sorteo aleatorio simulando lo que es un amigo secreto.
+
+Se utiliza CSS para diseño,Java Scrip para la lógica del sorteo y HTML para estructura de la página.
---
+++ app.js
@@ -0,0 +1,46 @@
+let listaNombres = [];
+
+function verificarNombre() {
+    let nombrePersona = document.getElementById('amigo').value;
+
+    if (nombrePersona !== "") {
+        listaNombres.push(nombrePersona);
+        actualizarLista();
+        document.getElementById('amigo').value = "";
+    } else {
+        alert('No puede quedar en blanco el espacio');
+    }
+}
+
+function actualizarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+
+    listaNombres.forEach(nombre => {
+        let li = document.createElement('li');
+        li.textContent = nombre;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+  if (listaNombres.length < 1) {
+      alert('No hay nombres para sortear');
+      return;
+  }
+
+  document.getElementById('listaAmigos').style.display = "none"; // Oculta la lista cuando ya se sortea
+
+  let resultadoLista = document.getElementById('resultado');
+  resultadoLista.innerHTML = ""; // Limpiar resultados anteriores
+
+  // Seleccionar un nombre aleatorio
+  let indiceAleatorio = Math.floor(Math.random() * listaNombres.length); // Índice aleatorio
+  let nombreSorteado = listaNombres[indiceAleatorio]; // Nombre seleccionado
+
+  // Mostrar el nombre sorteado
+  let li = document.createElement('li');
+  li.textContent = nombreSorteado;
+  resultadoLista.appendChild(li);
+}
+
